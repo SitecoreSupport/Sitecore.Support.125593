941137b64ddb159d3cc727cde13639993a4d5227
DK\mao <mao@sitecore.net>
2017-10-09 14:29:31 +0300

    Create new CacheStatus and RenderingsStatistics logs instead of overriding ones.

---
+++ src/Sitecore.Support.125593/Pipelines/HealthMonitor/HealthMonitor.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.IO;
+using Sitecore.Pipelines.HealthMonitor;
+using System;
+using System.IO;
+using Sitecore.Pipelines;
+
+namespace Sitecore.Support.Pipelines.HealthMonitor
+{
+  public class HealthMonitor : Sitecore.Pipelines.HealthMonitor.HealthMonitor
+  {
+    protected override void DumpInFile(string text)
+    {
+      string fileName = base.DumpFile.Replace("{datetime}", DateUtil.IsoNow)
+        .Replace("{date}", DateUtil.IsoNowDate)
+        .Replace("{time}", DateUtil.IsoNowTime);
+
+      FileUtil.EnsureFileFolder(fileName);
+      using (StreamWriter writer = File.CreateText(fileName))
+      {
+        writer.Write(text);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.125593/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.125593")]
+[assembly: AssemblyProduct("Sitecore.Support.125593")]
+[assembly: ComVisible(false)]
